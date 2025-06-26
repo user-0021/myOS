04f9252f0087aca91acd6aebc9e0ebfce0dda539
user-0021 <nabeyamaoden@gmail.com>
2025-06-26 07:28:32 +0000

    update conteiner

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ cp grub.cfg isofiles/boot/grub/
 cp kernel.elf isofiles/boot/
 grub-mkrescue -o os.iso isofiles/
 
-qemu-system-x86_64 -machine q35 -cpu qemu64 -m 256 -smp 2 -drive format=raw,file=os.iso -d cpu_reset,int,guest_errors -D qemu.log -monitor stdio
+if hash qemu-system-x86_64 2>/dev/null; then
+	qemu-system-x86_64 -machine q35 -cpu qemu64 -m 256 -smp 2 -drive format=raw,file=os.iso -d cpu_reset,int,guest_errors -D qemu.log -monitor stdio
+fi
